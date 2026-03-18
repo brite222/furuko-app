d312f96b134ea8431e8326414d8d8d27d1b9f5c0
brite222 <brighttosin222@gmail.com>
2026-03-18 16:12:11 +0100

    fix: serve frontend static files

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend')));  // ← ADD THIS
 
 const authRoutes = require("./routes/authRoutes");
 const protectedRoutes = require("./routes/protectedRoutes");
